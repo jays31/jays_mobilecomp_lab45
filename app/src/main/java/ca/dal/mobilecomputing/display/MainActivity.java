92b5191c4b8330f0d0398acce94449cc509312d6
Jay Sharma <jy741228@dal.ca>
2019-02-13 20:39:00 -0400

    Added bundle of data to Intent and pulled data from it

--- app/src/main/java/ca/dal/mobilecomputing/display/MainActivity.java
+++ app/src/main/java/ca/dal/mobilecomputing/display/MainActivity.java
@@ -23,14 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mActivity = this;
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         ListView studentList = findViewById(R.id.listview_students);
         final Database database = new Database();
+
         studentList.setAdapter(new StudentCourseAdapter(mActivity, database.getStudentModels(), true));
 
+        studentList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adaptor, View v, int listIndex, long arg3){
+                Intent detailsIntent = new Intent(MainActivity.this, StudentDetailActivity.class);
+                Bundle detailsBundle = new Bundle();
+                StudentModel currStudent = database.getStudentModels().get(listIndex);
+                String studentID = currStudent.getId();
+                String studentName = currStudent.getName();
+                int studentAge = currStudent.getAge();
 
+                detailsIntent.putExtra("studentID",studentID);
+                detailsBundle.putString("name",studentName);
+                detailsBundle.putInt("age",studentAge);
+                detailsIntent.putExtra("bundle",detailsBundle);
+                startActivity(detailsIntent);
+            }
+        });
     }
-
 }
